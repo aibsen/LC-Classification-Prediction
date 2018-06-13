7abcbde03713bbcc6deb356b1465ae7f2836f738
Amanda Ibsen <amanda.ibsen@gmail.com>
2018-06-13 14:15:21 +0100

    feature_plots

--- feature_plots.py
+++ feature_plots.py
@@ -24,13 +24,16 @@ def plot_pairs_of_classes(tagged_features, outputFile):
 if __name__ == "__main__":
     inputFile = "data/clean_tagged_features.csv"
     outputFile = "results/feature_plots/feature_plots"
+    outputFile2 = "results/feature_plots/feature_plots_classes"
 
     if len(sys.argv)>1:
         inputFile = "data/"+sys.argv[1]+".csv"
     if len(sys.argv)>2:
         outputFile = "results/"+sys.argv[2]
+    if len(sys.argv)>3:
+        outputFile2 = "results/"+sys.argv[3]
 
     tagged_features = pd.read_csv(inputFile, sep=",")
 
     plot_all_classes(tagged_features, outputFile)
-    # plot_pairs_of_classes(tagged_features, outputFile)
+    plot_pairs_of_classes(tagged_features, outputFile)
